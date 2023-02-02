88f9516a1be9f88258818ddcb617cc3f193928f6
Simon Prickett <simon@redislabs.com>
2023-02-02 15:51:48 +0000

    Work in progress adds exception handling.

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import network
 import secrets
 import time
-from picoredis import Redis
+from picoredis import Redis, RedisError
 
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
 
 while not wlan.isconnected() and wlan.status() >= 0:
+    # If wifi configuration is incorrect, or the specified SSID can't be found
+    # this will loop forever.  Consider adding logic to give up after a large
+    # number of attempts...
     print("Connecting to wifi...")
     time.sleep(0.5)
 
@@ -15,10 +18,22 @@ while not wlan.isconnected() and wlan.status() >= 0:
 r = Redis(host = secrets.REDIS_HOST, port = secrets.REDIS_PORT)
 
 if len(secrets.REDIS_PASSWORD) > 0:
-    # Authenticate to Redis.
-    r.auth(secrets.REDIS_PASSWORD)
+    # Authenticate to Redis, optionally with username.
+    if len(secrets.REDIS_USER) > 0:
+        r.auth(secrets.REDIS_USER, secrets.REDIS_PASSWORD)
+    else:
+        r.auth(secrets.REDIS_PASSWORD)
 
 while True:
-    response = r.ping()
-    print(response)
+    try:
+        response = r.ping()
+        print(response)
+    except RedisError as ex:
+        # Some exception occurred, for example a NOPERM error
+        # if using an ACL user that isn't allowed to run the 
+        # command that was attempted.  Example output:
+        #
+        # ('NOPERM', "this user has no permissions to run the 'get' command")
+        print(ex)
+
     time.sleep(2)
